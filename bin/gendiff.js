33a9e43182207879c8e882eeddf27faedc7c5ccc
dmitry <akulinind@gmail.com>
2020-06-15 13:37:53 +0300

    add compare of 2 json files

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import program from "commander";
-
+import {genDiff} from "./path.js";
 program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format [type]', 'output format')
-    .arguments('<filepath1> <filepath2>')
-    .parse(process.argv);
+     .arguments('<filepath1> <filepath2>')
+    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+
+program.parse(process.argv)
--- bin/path.js
+++ bin/path.js
@@ -1,5 +1,35 @@
 #!/usr/bin/env node
-
+import fs from "fs";
+import path from "path";
+import JSON from "JSON";
+import _ from "lodash";
 const currentPath = process.cwd();
 
-const getPathToFile = (currentPath, pathToFile) => path.resolve(currentPath, pathToFile);
+export const getAbsolutePath = (pathToFile) => {
+    return path.resolve(currentPath, pathToFile);
+}
+
+
+export const genDiff = (firstFilePath, secondFilePath) => {
+    const first = JSON.parse(fs.readFileSync(getAbsolutePath(firstFilePath), 'utf-8'));
+    const second = JSON.parse(fs.readFileSync(getAbsolutePath(secondFilePath), 'utf-8'));
+    return parseDiff(first, second);
+}
+
+const parseDiff = (obj1, obj2) => {
+    const mergedObject = {...obj1, ...obj2};
+    const result = Object.keys(mergedObject).map((key) => {
+        if (!_.has(obj1, key)) {
+            return ` + ${key}: ${obj2[key]}`;
+        }
+        if (!_.has(obj2, key)) {
+            return ` - ${key}: ${obj1[key]}`;
+        }
+        if (obj1[key] !== obj2[key]) {
+            return ` - ${key}: ${obj1[key]}\n + ${key}: ${obj2[key]}`;
+        }
+        return (`   ${key}: ${obj2[key]}`);
+    }).join('\n');
+    return `{\n${result}\n}`;
+}
+
