47012bde05ed790fab1a55dc58ab0d1e708b982c
dmitry <akulinind@gmail.com>
2020-06-15 17:08:56 +0300

    esling, codeclimate

---
+++ README.md
@@ -0,0 +1,2 @@
+[![Maintainability](https://api.codeclimate.com/v1/badges/ec2d362cf68403623883/maintainability)](https://codeclimate.com/github/akulahere/frontend-project-lvl2/maintainability)  
+[![Test Coverage](https://api.codeclimate.com/v1/badges/ec2d362cf68403623883/test_coverage)](https://codeclimate.com/github/akulahere/frontend-project-lvl2/test_coverage)  
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
-import program from "commander";
-import {genDiff} from "./path.js";
-program
-    .version('0.1.0')
-    .description('Compares two configuration files and shows a difference.')
-    .option('-f, --format [type]', 'output format')
-     .arguments('<filepath1> <filepath2>')
-    .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+import program from 'commander';
+import { genDiff } from './path';
 
-program.parse(process.argv)
+program
+  .version('0.1.0')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>')
+  .action((filepath1, filepath2) => console.log(genDiff(filepath1, filepath2)));
+
+program.parse(process.argv);
