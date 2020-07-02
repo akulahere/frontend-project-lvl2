bddad4f54148d976b4116b8c8425b9b77d072b52
dmitry <akulinind@gmail.com>
2020-07-02 11:51:11 +0300

    rename variables in tests

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -3,17 +3,17 @@ import path from 'path';
 import genDiff from '../src';
 
 const inputFormats = ['json', 'ini', 'yml'];
-const formats = ['json', 'stylish', 'plain'];
+const outputFormats = ['json', 'stylish', 'plain'];
 
 const getFixturePath = (filename) => path.join('__tests__', '__fixtures__', filename);
 
 const getExpectedResult = (format) => fs.readFileSync(getFixturePath(`${format}ExpectedResult`), 'utf-8');
 
-describe.each(formats)('%s', (format) => {
+describe.each(outputFormats)('%s', (outputFormat) => {
   test.each(inputFormats)('%s', (inputFormat) => {
     const before = getFixturePath(`before.${inputFormat}`);
     const after = getFixturePath(`after.${inputFormat}`);
-    const actual = genDiff(before, after, format);
-    expect(actual).toEqual(getExpectedResult(format));
+    const actual = genDiff(before, after, outputFormat);
+    expect(actual).toEqual(getExpectedResult(outputFormat));
   });
 });
