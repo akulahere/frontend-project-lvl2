456c6b8226a31bc8e6d66190b04bef562b6787e9
dmitry <akulinind@gmail.com>
2020-07-10 15:50:49 +0300

    remove json formatter

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -1,11 +1,10 @@
 import makePlain from './plain.js';
 import makeStylish from './stylish.js';
-import makeJson from './json.js';
 
 const formatters = {
   stylish: (diff) => makeStylish(diff),
   plain: (diff) => makePlain(diff),
-  json: (diff) => makeJson(diff),
+  json: (diff) => JSON.stringify(diff),
 };
 
 const render = (diff, renderFormat) => formatters[renderFormat](diff);
