86f6befcaa9f1aedfc5fe8f345bf2a1503e43ae0
dmitry <akulinind@gmail.com>
2020-07-21 18:04:26 +0300

    fix formatting value in stylish

--- src/formatters/stylish.js
+++ src/formatters/stylish.js
@@ -4,16 +4,19 @@ const baseIndent = '    ';
 const makeIndent = (depth) => baseIndent.repeat(depth);
 
 const formatValue = (value, depth) => {
-  if (typeof value === 'string') {
-    return value;
+  if (_.isObject(value)) {
+    const indent = makeIndent(depth + 1);
+    const result = Object.entries(value)
+      .map(([key, keyValue]) => {
+        const calcValue = _.isObject(keyValue) ? formatValue(keyValue, depth + 1) : keyValue;
+        return `${indent}    ${key}: ${calcValue}`;
+      });
+    return `{\n${result.join('\n')}\n${indent}}`;
   }
-  const indent = makeIndent(depth + 1);
-  const result = Object.entries(value)
-    .map(([key, keyValue]) => {
-      const calcValue = _.isObject(keyValue) ? formatValue(keyValue, depth + 1) : keyValue;
-      return `${indent}    ${key}: ${calcValue}`;
-    });
-  return `{\n${result.join('\n')}\n${indent}}`;
+  if (typeof value === 'string') {
+    return `${value}`;
+  }
+  return value;
 };
 
 const makeStylish = (diff, depth = 0) => {
