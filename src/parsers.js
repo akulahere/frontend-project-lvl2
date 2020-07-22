ebe86c035ec010a0362ba5abf978fcb81a22a692
dmitry <akulinind@gmail.com>
2020-07-22 17:39:11 +0300

    update ini parser

--- src/parsers.js
+++ src/parsers.js
@@ -1,10 +1,22 @@
 import yaml from 'js-yaml';
 import ini from 'ini';
 
+const parseIni = (content) => {
+  const parseContent = ini.parse(content);
+  const contentConverted = JSON.stringify(parseContent, (key, value) => {
+    const valueConvert = parseFloat(value);
+    if (!Number.isNaN(valueConvert)) {
+      return valueConvert;
+    }
+    return value;
+  });
+  return JSON.parse(contentConverted);
+};
+
 const parsers = {
   json: JSON.parse,
   yml: yaml.safeLoad,
-  ini: ini.parse,
+  ini: parseIni,
 };
 
 const parse = (content, format) => {
